6f867298d2c53f936b0fa8396342b0cdcb97b575
Dileep Chaudhary <dileepchaudhary@Dileeps-MacBook-Air.local>
2024-11-27 23:25:01 +0530

    First commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import PersonalPortfolio from "./home/personal-portfolio/page";
+
+export const metadata = {
+  title: "Dileep Chaudhary - Personal",
+};
+const MainRoot = () => {
+  return <PersonalPortfolio />;
+};
+
+export default MainRoot;
